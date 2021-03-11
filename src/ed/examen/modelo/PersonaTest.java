bc3c2a110ad6c373589adc9772b509cea571541f
Clase <clase@MacBook-Pro-de-Javier.local>
2021-03-11 17:20:39 +0100

    Implementacion test PersonaStringStringString

---
+++ src/ed/examen/modelo/PersonaTest.java
@@ -0,0 +1,49 @@
+package ed.examen.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonaTest {
+
+	@Test
+	public void testPersonaStringStringString() {
+		Persona p = new Persona("18086912b", "Javier", "Marmol");
+		assertEquals("18086912b",p.getDni());
+		assertEquals("Javier",p.getNombre());
+		assertEquals("Marmol",p.getApellido1());
+	}
+	
+	@Test
+	public void testGetDni() {
+		
+	}
+
+	@Test
+	public void testSetDni() {
+		
+		
+	}
+
+	@Test
+	public void testGetNombre() {
+		
+	}
+
+	@Test
+	public void testSetNombre() {
+		
+	}
+
+	@Test
+	public void testGetApellido1() {
+		
+	}
+
+	@Test
+	public void testSetApellido1() {
+		
+		
+	}
+
+}
